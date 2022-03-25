1099a56f1dcfa783ee33b353f17962f3e48d9999
charlesjbasedow <cbasedow@outlook.com>
2022-03-25 15:11:03 -0400

    goalcreate

---
+++ src/services/goal.js
@@ -0,0 +1,38 @@
+const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/goals`
+
+function create(goal) {
+  return fetch(BASE_URL, {
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify(goal)
+  })
+  .then(res => res.json())
+}
+
+function getAll() {
+  return fetch(BASE_URL)
+  .then(res => res.json())
+}
+
+function deleteOne(id) {
+  return fetch(`${BASE_URL}/${id}`, {
+    method: 'DELETE'
+  })
+  .then(res => res.json())
+}
+
+function update(goal) {
+  return fetch(`${BASE_URL}/${goal._id}`, {
+    method: 'PUT',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify(goal)
+  })
+  .then(res => res.json())
+}
+
+export {
+  create,
+  getAll,
+  deleteOne,
+  update
+}

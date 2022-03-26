c0ddedf4a19d0a639a49235ac4a41d834b1030a5
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-03-25 20:06:09 -0400

    complete income index functionality

--- src/components/IncomeCard/IncomeCard.jsx
+++ src/components/IncomeCard/IncomeCard.jsx
@@ -1,9 +1,11 @@
-const IncomeCard = (incomes) => {
+const IncomeCard = (props) => {
   return (  
     <>
     <h1>Income Card</h1>
     <div>
-      
+      <h3>{props.income.name}</h3>
+      <p>{props.income.amount}</p>
+      <p>{props.income.date}</p>
     </div>
     </>
   );
--- src/pages/Transactions/Transactions.jsx
+++ src/pages/Transactions/Transactions.jsx
@@ -1,14 +1,12 @@
+import IncomeCard from "../../components/IncomeCard/IncomeCard";
+
 const Transactions = (props) => {
   return (  
     <>
     <h1>Transactions</h1>
     <div>
       {props.incomes.map(income => (
-        <div key={income._id}>
-          <p>{income.name}</p>
-          <p>{income.amount}</p>
-          <p>{income.date}</p>
-        </div>
+        <IncomeCard key={income._id} income={income} />
       ))}
     </div>
     </>

805f392b4deb4437b414ee075d3fdcb3c1de2611
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-03-26 21:27:12 -0400

    add styling to income and expense cards

--- src/components/IncomeCard/IncomeCard.jsx
+++ src/components/IncomeCard/IncomeCard.jsx
@@ -1,13 +1,15 @@
+import './IncomeExpenseCard.css'
+
 function IncomeCard({income, handleDeleteIncome, user}) {
   return (  
     <>
-    <h1>Income Card</h1>
     {income.owner._id === user.profile ?
-    <div>
-      <h3>{income.name}</h3>
-      <p>{income.amount}</p>
-      <p>{income.date}</p>
-      <button onClick={() => handleDeleteIncome(income._id)} >X</button>
+    <div className='card'>
+      {/* <h3>income card</h3> */}
+      <p className='name'>{income.name}</p>
+      <p className='amount'>+ ${income.amount}</p>
+      <p className='date'>{income.date}</p>
+      <button className='delete-btn' onClick={() => handleDeleteIncome(income._id)} >X</button>
     </div>
     :
     <p></p>

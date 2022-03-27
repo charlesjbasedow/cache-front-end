7506141c2d811bd483519686a5426da780c0ba71
charlesjbasedow <cbasedow@outlook.com>
2022-03-27 02:20:31 -0400

    goal-auth add

--- src/services/goals.js
+++ src/services/goals.js
@@ -1,33 +1,47 @@
+import * as tokenService from './tokenService'
 const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/goals`
 
 function create(goal) {
   return fetch(BASE_URL, {
     method: 'POST',
-    headers: {'content-type': 'application/json'},
+    headers: {
+      'content-type': 'application/json',
+      'Authorization': `Bearer ${tokenService.getToken()}`
+    },
     body: JSON.stringify(goal)
   })
   .then(res => res.json())
 }
 
 function getAll() {
-  return fetch(BASE_URL)
+  return fetch(BASE_URL, {
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`
+    }
+  })
   .then(res => res.json())
 }
 
 function deleteOne(id) {
   return fetch(`${BASE_URL}/${id}`, {
-    method: 'DELETE'
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`
+    }
   })
   .then(res => res.json())
 }
 
+
 function update(goal) {
-  return fetch(`${BASE_URL}/${goal._id}`, {
+  return fetch(`${BASE_URL}/${goal.get('_id')}`, {
     method: 'PUT',
-    headers: {'content-type': 'application/json'},
-    body: JSON.stringify(goal)
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`
+    },
+    body: goal
   })
-	.then(res => res.json())
+  .then(res => res.json())
 }
 
 export {

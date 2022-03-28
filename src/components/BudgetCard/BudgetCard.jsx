80650ff49842f66ddfc205c006fba3ec70beba62
binod164 <binod7_grg@hotmail.com>
2022-03-28 13:40:54 -0500

    function to add total for each category

--- src/components/BudgetCard/BudgetCard.jsx
+++ src/components/BudgetCard/BudgetCard.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 
-function BudgetCard({budget, handleDeleteBudget,totalExpense, totalIncome, user,healthExpense}) {
+function BudgetCard({budget, user, handleDeleteBudget,totalExpense, healthExpense,housingExpense, groceryExpense,billsExpense, travelExpense}) {
 
   return (  
     <>
@@ -11,9 +11,13 @@ function BudgetCard({budget, handleDeleteBudget,totalExpense, totalIncome, user,
       <p>health limit: {budget.healthLimit}</p>
       <p>health expense: {healthExpense}</p>
       <p>housing limit: {budget.housingLimit}</p>
+      <p>housing expense: {housingExpense}</p>
       <p>grocery limit: {budget.groceryLimit}</p>
+      <p>grocery expense: {groceryExpense}</p>
       <p>bills limit: {budget.billsLimit}</p>
+      <p>bills expense: {billsExpense}</p>
       <p>travel limit: {budget.travelLimit}</p>
+      <p>travel expense: {travelExpense}</p>
       <Link
           className='btn btn-sm btn-warning'
           to='/edit-budget'

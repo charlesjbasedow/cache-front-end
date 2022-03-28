5d0457a4b339a439aa37de87d7aebd0cfc36cf47
binod164 <binod7_grg@hotmail.com>
2022-03-27 21:57:35 -0500

    make changes in editbudget page

--- src/pages/EditBudget/EditBudget.jsx
+++ src/pages/EditBudget/EditBudget.jsx
@@ -26,61 +26,109 @@ const EditBudget = (props) => {
 
 
   return (
-    <>
-      <h1>Edit Budget</h1>
-      <form autoComplete="off" ref={formElement} onSubmit={handleSubmit}>
+		<>
+			<h1>Add Budget</h1>
+			<form autoComplete="off" ref={formElement} onSubmit={handleSubmit}>
 				<div className="form-group mb-4">
 					<label htmlFor="total-input" className="form-label">
-						Total Limit
+							Total Limit
 					</label>
 					<input 
 						type="number"
 						className="form-control"
 						id="total-input"
 						name="totalLimit"
-            value={formData.totalLimit} 
-            onChange={handleChange}
+						value={formData.totalLimit}
+						onChange={handleChange}
 					/>
-				</div>
+					</div>
+
 				<div className="form-group mb-3">
-					<label htmlFor="budget-input" className="form-label">
-						Budget Category (required)
-					</label>
-					<select name="category"  value={formData.category} onChange={handleChange} >
-            <option name="Health">Health</option>
-            <option name="Housing">Housing</option>
-            <option name="Grocery">Grocery</option>
-            <option name="Bills">Bills</option>
-            <option name="Travel">Travel</option>
-            <option name="Other">Other</option>
-          </select>
-				</div>
-				<div className="form-group mb-3">
-					<label htmlFor="categorytotal-input" className="form-label">
-						Category Limit (required)
+					<label htmlFor="health-limit-input" className="form-label">
+						Health Limit
 					</label>
 					<input 
 						type="number"
 						className="form-control"
-						id="categorytotal-input"
-						name="categoryLimit"
-            value={formData.categoryLimit} 
-            onChange={handleChange}
+						id="health-limit-input"
+						name="healthLimit"
+						value={formData.healthLimit}
+						onChange={handleChange}
+					/>
+				</div>
+
+				<div className="form-group mb-3">
+					<label htmlFor="categorytotal-input" className="form-label">
+						Housing Limit
+					</label>
+					<input 
+						type="number"
+						className="form-control"
+						id="housing-limit-input"
+						name="housingLimit"
+						value={formData.housingLimit}
+						onChange={handleChange}
 						required
 					/>
 				</div>
-        <div className="d-grid">
+
+				<div className="form-group mb-3">
+					<label htmlFor="grocery-limit-input" className="form-label">
+						Grocery Limit
+					</label>
+					<input 
+						type="number"
+						className="form-control"
+						id="grocery-limit-input"
+						name="groceryLimit"
+						value={formData.groceryLimit}
+						onChange={handleChange}
+						required
+					/>
+				</div>
+
+				<div className="form-group mb-3">
+					<label htmlFor="bills-limit-input" className="form-label">
+						Bills Limit
+					</label>
+					<input 
+						type="number"
+						className="form-control"
+						id="bills-limit-input"
+						name="billsLimit"
+						value={formData.billsLimit}
+						onChange={handleChange}
+						required
+					/>
+				</div>
+
+				<div className="form-group mb-3">
+					<label htmlFor="travel-limit-input" className="form-label">
+						Travel Limit
+					</label>
+					<input 
+						type="number"
+						className="form-control"
+						id="travel-limit-input"
+						name="travelLimit"
+						value={formData.travelLimit}
+						onChange={handleChange}
+						required
+					/>
+				</div>
+
+				<div className="d-grid">
 					<button
 						type="submit"
 						className="btn btn-primary btn-fluid"
-            disabled={!validForm}
+						disabled={!validForm}
 					>
-						Edit Budget
+						Add Budget
 					</button>
 				</div>
 			</form>
-    </>
-    );
+		</>
+  );
 }
 
 export default EditBudget;

a10e8e95b24dafdfbc60e168b604244d711a467b
binod164 <binod7_grg@hotmail.com>
2022-03-30 15:33:29 -0500

    calculate  amountleft on landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,17 +4,15 @@ import DoughnutChart from '../../components/DoughnutChart/DoughnutChart'
 const Landing = (props) => {
   const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
-  const d = new Date();
-  let currentMonth = month[d.getMonth()];
+  const d = new Date()
+  let currentMonth = month[d.getMonth()]
 
-  let currentBudget = props?.budgets[props.budgets.length-1]
-
-  console.log(currentBudget)
+  let totalLimit = props?.budgets[props.budgets.length-1]?.totalLimit
 
   return (
     <main className={styles.container}>
       <h1>{currentMonth} Spending Overview</h1>
-        {/* <h3>You have {currentBudget.totalLimit - props.totalExpense} remaining in this month's budget</h3> */}
+        <h3>You have {totalLimit - props.totalExpense} remaining in this month's budget</h3>
       <div>
         <DoughnutChart 
         totalExpense={props.totalExpense} 

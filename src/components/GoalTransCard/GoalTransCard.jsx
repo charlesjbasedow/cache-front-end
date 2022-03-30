93801a77bac58aac17217639c8253e8d887f42be
charlesjbasedow <cbasedow@outlook.com>
2022-03-30 11:30:00 -0400

    only show goal when more than zero

--- src/components/GoalTransCard/GoalTransCard.jsx
+++ src/components/GoalTransCard/GoalTransCard.jsx
@@ -2,10 +2,11 @@ import { Link } from 'react-router-dom'
 import './GoalTransCard.css'
 import GoalProgressBar from '../GoalProgressBar/GoalProgressBar';
 
+
 function GoalTransCard ({goal,user}) {
   return ( 
     <>
-    {goal.owner._id === user.profile ?
+    {goal.owner._id === user.profile && goal.currentAmount > 0 ?
     <div className='goal-container'>
       <div>
       <GoalProgressBar />

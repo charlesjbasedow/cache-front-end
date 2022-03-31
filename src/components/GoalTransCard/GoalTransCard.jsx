5946986e59d476a1b8b9a5dfc97063e5a4751ffb
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-03-31 12:39:06 -0400

    fix header nav

--- src/components/GoalTransCard/GoalTransCard.jsx
+++ src/components/GoalTransCard/GoalTransCard.jsx
@@ -9,8 +9,8 @@ function GoalTransCard ({goal,user}) {
     {goal.owner._id === user.profile && goal.currentAmount > 0 ?
     <div className='card'>
       <SavingsIcon fontSize="large" className='goall-icon' />
-      <p className='goall-name'>{goal.name}</p>
-      <p className='sent'>Sent to Goal</p>
+      <p className='goall-name'>{goal.name} Goal</p>
+      <p className='sent'>Sent to Savings</p>
       <p className='amount'>- ${goal.currentAmount}</p>
     </div>
     :
